159e65bbb3fa12ce6b7d25d66fdf85337f33589f
rust <zkshangxiao@126.com>
2019-07-06 09:05:35 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+
+// 实例化 Koa
+const app = new Koa()
+const router = new Router()
+
+// 路由
+router.get('/', async ctx => {
+    ctx.body = { msg: 'Koa Interface' }
+})
+
+// 配置路由
+app.use(router.routes()).use(router.allowedMethods())
+
+// 定义端口号
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`server started on ${port}`)
+})
+
